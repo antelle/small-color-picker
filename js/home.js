99149915717ccc05f1cbb799168461c24f2c97e0
antelle <dmitryvit@gmail.com>
2013-05-18 21:23:34 +0400

    added GitHub pages

---
+++ js/home.js
@@ -0,0 +1,50 @@
+$(function() {
+    $("body").addClass("no-bg");
+    $(window).load(function() {
+        $("body").removeClass("no-bg");
+    });
+
+    try {
+        var colorPicker = window.colorPicker = new SmallColorPicker.CirclePicker({
+            placement: {
+                position: "static",
+                parent: $("#color-selector-box")
+            },
+            colors: {
+                colorOld: "#990e8d",
+                colorNew: "#6d1f7a"
+            },
+            behavior: {
+                hideOnSelect: false
+            },
+            events: {
+                ok: function(color) {
+                    showColorPickerText("Selected color: " + color);
+                },
+                cancel: function(color) {
+                    showColorPickerText("Cancelled");
+                }
+            }
+        });
+        colorPicker.show();
+    } catch (err) {
+        $("#color-selector-box")
+            .addClass("text-error")
+            .html("Error loading SmallColorPicker (" + err + ")");
+    }
+
+    $(".color-btn").click(function() {
+        var cls = this.className.replace("color-btn ", "");
+        var color = /#\w+/g.exec($("b", this).attr("style"))[0];
+        $("#span-code-style span").html(cls);
+        $("#span-code-color span").html(color);
+        colorPicker.setColors(color);
+    });
+
+    prettyPrint();
+});
+
+function showColorPickerText(text) {
+    $("#color-selector-text").html(text).stop().show().css({opacity: 1}).fadeOut(2000);
+}
+
